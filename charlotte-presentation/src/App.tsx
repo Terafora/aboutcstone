92c19ef9c1b3fbe154ad71ad680a78e1103c3779
Charlotte Stone <charlie.stone649@gmail.com>
2025-05-15 16:34:41 +0100

    Refactor App component and add Intro slide component and keyboard nav

--- charlotte-presentation/src/App.tsx
+++ charlotte-presentation/src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import Intro from './slides/Intro'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+
+  //This is for using arrow keys to move between "slides"
+  useEffect(() => {
+    const handleKey = (e: KeyboardEvent) => {
+      if(e.key === "ArrowRight"){
+        setCurrentSlide((prev) => Math.min(prev + 1, slides.length -1));
+      };
+      if(e.key === "ArrowLeft"){
+        setCurrentSlide((prev) => Math.max(prev -1, 0));
+      };
+      window.addEventListener("keydown", handleKey);
+      return() => window.removeEventListener("keydown", handleKey);
+    }
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Intro/>
     </>
   )
 }
---
+++ charlotte-presentation/src/slides/Intro.tsx
@@ -0,0 +1,9 @@
+const Intro = () => {
+  return (
+    <div>
+      <h2>Placeholder text :)</h2>
+    </div>
+  );
+};
+
+export default Intro;
