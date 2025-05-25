fb06179a710443dbef4a5d5976772f3080f56f31
Charlotte Stone <charlie.stone649@gmail.com>
2025-05-25 12:50:29 +0100

    Update project descriptions and translations in multiple languages; enhance Projects component for better localization

--- charlotte-presentation/src/slides/Projects.tsx
+++ charlotte-presentation/src/slides/Projects.tsx
@@ -4,39 +4,40 @@ import PEVImg from '../assets/bg-items/PEV.png';
 import YS from '../assets/bg-items/yomisaverb.png';
 import MMT from '../assets/bg-items/my_mix_tape_logo.svg'
 
-const projects = [
-    {
-        title: "LangBloom",
-        date: "2025",
-        description: 'A full-stack SaaS for assisting with the admin side of language teaching. <br/> Currently a WIP so link withheld.<br/>React, TypeScript,PWA,Nhost (A very AI assisted project)',
-        img: "",
-        link: ""
-    },
-    {
-        title: "Project Exit Velocity",
-        date: "2024 - 2025",
-        description: "A full-stack MERN blog for sharing progress on personal projects and learning.<br/>MERN, JWT, Typsecript",
-        img: PEVImg,
-        link: "https://github.com/Terafora/project-exit-velocity-blog"
-    },
-    {
-        title: "YomiSaver",
-        date: "2024",
-        description: "A Chrome extension for simplifying the learning process for Japanese.<br/>HTML, CSS, Vanilla JavaScript",
-        img: YS,
-        link: "https://github.com/Terafora/Yomisaver-Free"
-    },
-    {
-        title: "My Mixtape",
-        date: "2022",
-        description: "A full-stack site for creating and sharing mixtapes to share with others.<br/>Python, Django",
-        img: MMT,
-        link: "https://my-mix-tapes-a7ee13848429.herokuapp.com"
-    },   
-];
-
 const Projects = () => {
     const { t } = useTranslation();
+
+    const projects = [
+        {
+            title: t("slides.slide3"),
+            date: "2025",
+            description: t("slide3.lbdesc"),
+            img: "",
+            link: ""
+        },
+        {
+            title: "Project Exit Velocity",
+            date: "2024 - 2025",
+            description: t("slide3.pevdesc"),
+            img: PEVImg,
+            link: "https://github.com/Terafora/project-exit-velocity-blog"
+        },
+        {
+            title: "YomiSaver",
+            date: "2024",
+            description: t("slide3.ysdesc"),
+            img: YS,
+            link: "https://github.com/Terafora/Yomisaver-Free"
+        },
+        {
+            title: "My Mixtape",
+            date: "2022",
+            description: t("slide3.mmdesc"),
+            img: MMT,
+            link: "https://my-mix-tapes-a7ee13848429.herokuapp.com"
+        },   
+    ];
+
     return (
         <div className="diagonal-scroll-wrapper">
             <div className="timeline-container">
