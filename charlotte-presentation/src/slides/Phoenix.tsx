30c3fd949d7e01fc6bb3cb9c3ad526e286e3e28e
Charlotte Stone <charlie.stone649@gmail.com>
2025-05-29 10:59:57 +0100

    Add Phoenix slide component and styles; create empty WorkTimeline and ProjectTimeline components

--- charlotte-presentation/src/slides/Phoenix.tsx
+++ charlotte-presentation/src/slides/Phoenix.tsx
@@ -1,10 +1,12 @@
 import {useTranslation} from 'react-i18next';
+import './Phoenix.scss'
 
 const Phoenix = () => {
   const {t} = useTranslation()
   return (
     <div>
       <h2>{t("phoenix")}</h2>
+      
     </div>
   );
 };
