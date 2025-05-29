a6b2302d229cbc6e9d1f4caf295b3aa321b375a0
terafora <charlie.stone649@gmail.com>
2025-05-29 12:36:13 +0100

    Remove WorkTimeline and ProjectTimeline components; update Phoenix slide with new content and styles; enhance localization in multiple languages

--- charlotte-presentation/src/slides/Phoenix.tsx
+++ charlotte-presentation/src/slides/Phoenix.tsx
@@ -1,13 +1,19 @@
 import {useTranslation} from 'react-i18next';
-import './Phoenix.scss'
+import './WhyIT.scss'
 
 const Phoenix = () => {
-  const {t} = useTranslation()
+  const {t} = useTranslation();
   return (
-    <div>
-      <h2>{t("phoenix")}</h2>
-      
-    </div>
+    <section>
+      <h1> </h1>
+      <ul className="reasons">
+        <li>{t('slide6.reason1')}</li>
+        <li>{t('slide6.reason2')}</li>
+        <li>{t('slide6.reason3')}</li>
+        <li>{t('slide6.reason4')}</li>
+        <li>{t('slide6.reason5')}</li>
+      </ul>
+    </section>
   );
 };
 
